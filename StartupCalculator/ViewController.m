6e1ffafbaba81845e019b6e5c15930795b5e894f
ryanleeallred <ryanleeallred@gmail.com>
2014-07-10 18:31:33 -0600

    Initial Commit

---
+++ StartupCalculator/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  StartupCalculator
+//
+//  Created by Ryan Allred on 7/10/14.
+//  Copyright (c) 2014 Ryan Allred. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+            
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
